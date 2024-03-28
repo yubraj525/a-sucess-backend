090d34ff5109f900a97ef7969ecf5ce3081f508d
yubraj525 <yubrajkhatri525@gmail.com>
2024-03-28 20:51:02 +0545

    test to testing

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect('localhost', 'root', '', 'test'); // server username password  table namethis line is use to connect the htnl page into database  as we have alreasy given a data base name test and table governn
+$con=mysqli_connect('localhost', 'root', '', 'testing'); // server username password  table namethis line is use to connect the htnl page into database  as we have alreasy given a data base name test and table governn
 // if($con)
 // {
 //     echo'<script> alert("connected")</script>';
